654d5ee26bd6fb160ce55ce25e610f84c32563f8
Frederic Marion-Poll <marion_poll@hotmail.com>
2021-09-23 14:54:31 +0200

    renaming

--- Program/DataMEA/dbWave/SaveDataFile.cs
+++ Program/DataMEA/dbWave/SaveDataFile.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using MEATaste.Annotations;
@@ -9,31 +8,31 @@ namespace MEATaste.DataMEA.dbWave
 {
     public class DataFileWriter
     { 
-        private const int DEVID				    = 4;	// device_id.........AD card id;
-        private const int DEVFLAGS				= 6;	// device_flags......type of acquisition;
-        private const int SCNCNT				= 8;	// scan_count .......number of acq channels;
-        private const int CHANLST				= 10;	// channel_list[8] ..input channel list;
-        private const int TRIGGER_MODE			= 26;	// trigger_mode......AD trigger mode [0..];
-        private const int TRIGGER_CHAN			= 28;	// trigger_chan......AD trigger channel [0..7];
-        private const int TRIGGER_THRESHOLD	    = 30;	// trigger_treshold..AD trig threshold value [binary];
-        private const int TRIGGER_POLARITY		= 32;	// trigger_polarity;
-        private const int GAINLST				= 42;	// gain_list[8]...... gain channel list;
-        private const int CHANCOM				= 74;	// channel_comment[8][40];
-        private const int ACQDATE				= 714;	// acq_date[10];
-        private const int ACQTIME				= 724;	// acq_time[10];
-        private const int TIMING				= 736;	// timing_source;
-        private const int CLKPER				= 738;	// clock_period;
-        private const int SAMCNT				= 742;  // sample_count;
-        private const int RDCNT				    = 746;	// read_count;
-        private const int ACQCOM				= 750;	// acq_comment[80];
-        private const int VERSION				= 830;	// version;
-        private const int CYBERA320			    = 832;	// CyberA320;
-        private const int CYBER_1				= 840;	// cyber1
-        private const int CYBER_2				= 868;  // cyber2
-        private const int XGAIN				    = 914;  // xgain_list[8]
+        private const int Devid				    = 4;	// device_id.........AD card id;
+        private const int Devflags				= 6;	// device_flags......type of acquisition;
+        private const int Scncnt				= 8;	// scan_count .......number of acq channels;
+        private const int Chanlst				= 10;	// channel_list[8] ..input channel list;
+        private const int TriggerMode			= 26;	// trigger_mode......AD trigger mode [0..];
+        private const int TriggerChan			= 28;	// trigger_chan......AD trigger channel [0..7];
+        private const int TriggerThreshold	    = 30;	// trigger_treshold..AD trig threshold value [binary];
+        private const int TriggerPolarity		= 32;	// trigger_polarity;
+        private const int Gainlst				= 42;	// gain_list[8]...... gain channel list;
+        private const int Chancom				= 74;	// channel_comment[8][40];
+        private const int Acqdate				= 714;	// acq_date[10];
+        private const int Acqtime				= 724;	// acq_time[10];
+        private const int Timing				= 736;	// timing_source;
+        private const int Clkper				= 738;	// clock_period;
+        private const int Samcnt				= 742;  // sample_count;
+        private const int Rdcnt				    = 746;	// read_count;
+        private const int Acqcom				= 750;	// acq_comment[80];
+        private const int Version				= 830;	// version;
+        private const int Cybera320			    = 832;	// CyberA320;
+        private const int Cyber1				= 840;	// cyber1
+        private const int Cyber2				= 868;  // cyber2
+        private const int Xgain				    = 914;  // xgain_list[8]
 
-        private const int ACQCOM_LEN = 80;
-        private const int DATA = 1024;
+        private const int AcqcomLen = 80;
+        private const int Data = 1024;
 
         public bool SaveCurrentElectrodeDataToAtlabFile(MeaExperiment experiment, ElectrodeProperties electrode,
             ElectrodeDataBuffer electrodeData)
@@ -86,40 +85,40 @@ namespace MEATaste.DataMEA.dbWave
         {
             binaryWriter.Seek(0, SeekOrigin.Begin);
             binaryWriter.Write(0xAAAA);
-            binaryWriter.Seek(DEVID, SeekOrigin.Begin); 
+            binaryWriter.Seek(Devid, SeekOrigin.Begin); 
             binaryWriter.Write((short)0); 
-            binaryWriter.Seek(SCNCNT, SeekOrigin.Begin); 
+            binaryWriter.Seek(Scncnt, SeekOrigin.Begin); 
             binaryWriter.Write((short)1);
-            binaryWriter.Seek(CHANLST, SeekOrigin.Begin);
+            binaryWriter.Seek(Chanlst, SeekOrigin.Begin);
             binaryWriter.Write((short)electrode.Channel);
-            binaryWriter.Seek(GAINLST, SeekOrigin.Begin);
+            binaryWriter.Seek(Gainlst, SeekOrigin.Begin);
             binaryWriter.Write((short)1);
-            binaryWriter.Seek(CHANCOM, SeekOrigin.Begin);
+            binaryWriter.Seek(Chancom, SeekOrigin.Begin);
             binaryWriter.Write(electrode.Electrode.ToString().ToCharArray());
 
-            binaryWriter.Seek(ACQDATE, SeekOrigin.Begin);
+            binaryWriter.Seek(Acqdate, SeekOrigin.Begin);
             var timeStart = experiment.Descriptors.TimeStart;
             var acqDate = timeStart.ToString(@"MM'/'dd'/'yyyy HH:mm:ss");
             binaryWriter.Write(acqDate.ToCharArray());
 
-            binaryWriter.Seek(CLKPER, SeekOrigin.Begin);
+            binaryWriter.Seek(Clkper, SeekOrigin.Begin);
             var clockperiod = 4E6f / experiment.Descriptors.SamplingRate;
             binaryWriter.Write((Int32)clockperiod);
 
             var length = electrodeData.RawSignalUShort.LongLength;
-            binaryWriter.Seek(SAMCNT, SeekOrigin.Begin); 
+            binaryWriter.Seek(Samcnt, SeekOrigin.Begin); 
             binaryWriter.Write((Int32)length);
 
-            binaryWriter.Seek(ACQCOM, SeekOrigin.Begin);
+            binaryWriter.Seek(Acqcom, SeekOrigin.Begin);
             binaryWriter.Write(electrode.Electrode.ToString().ToCharArray());
 
-            binaryWriter.Seek(ACQCOM + 20, SeekOrigin.Begin);
+            binaryWriter.Seek(Acqcom + 20, SeekOrigin.Begin);
             binaryWriter.Write(("X=" + electrode.XuM).ToCharArray());
 
-            binaryWriter.Seek(ACQCOM + 30, SeekOrigin.Begin);
+            binaryWriter.Seek(Acqcom + 30, SeekOrigin.Begin);
             binaryWriter.Write(("Y=" + electrode.YuM).ToCharArray());
 
-            binaryWriter.Seek(XGAIN, SeekOrigin.Begin);
+            binaryWriter.Seek(Xgain, SeekOrigin.Begin);
             double xgain = 20/(4096 * experiment.Descriptors.Lsb);
             binaryWriter.Write((float)xgain);
         }
@@ -128,7 +127,7 @@ namespace MEATaste.DataMEA.dbWave
         {
             if (electrodeData == null) throw new ArgumentNullException(nameof(electrodeData));
 
-            binaryWriter.Seek(DATA, SeekOrigin.Begin);
+            binaryWriter.Seek(Data, SeekOrigin.Begin);
             const short delta = 2048 - 512;
             
             foreach (var value in electrodeData.RawSignalUShort)
