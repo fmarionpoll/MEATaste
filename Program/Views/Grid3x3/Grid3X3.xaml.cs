73c8262732829f9a9ef62f8ce6f9704ac0c5b5b8
Frederic Marion-Poll <marion_poll@hotmail.com>
2021-10-17 22:57:02 +0200

    create Grid3X3 files

---
+++ Program/Views/Grid3x3/Grid3X3.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MEATaste.Views.Grid3x3
+{
+    /// <summary>
+    /// Interaction logic for Grid3X3.xaml
+    /// </summary>
+    public partial class Grid3X3 : UserControl
+    {
+        public Grid3X3()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Program/Views/Grid3x3/Grid3X3Controller.cs
@@ -0,0 +1,16 @@
+﻿using MEATaste.Infrastructure;
+
+namespace MEATaste.Views.Grid3x3
+{
+    class Grid3X3Controller
+    {
+        public Grid3X3Model Model { get; }
+        private readonly ApplicationState state;
+
+        public Grid3X3Controller(ApplicationState state, IEventSubscriber eventSubscriber)
+        {
+            this.state = state;
+            Model = new Grid3X3Model();
+        }
+    }
+}
